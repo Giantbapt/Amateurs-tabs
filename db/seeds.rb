105519a775f7d073cc2dea7e515e1b2b5096b6c7
exec zsh <exec zsh>
2023-10-23 00:22:46 +0200

    bdd

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require "csv"
+
+filepath = "data/names.csv"
+csv_text = File.read(Rails.root.join(filepath))
+puts csv_text
+csv = CSV.parse(csv_text, :headers => true)
+
+csv.each do |row|
+  puts "hello"
+  a = Athlete.new
+  a.name = row['name']
+  a.save
+  puts a.name
+
+end

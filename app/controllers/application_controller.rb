df062eefe36536843686646938cd77806940c48e
exec zsh <exec zsh>
2023-10-23 00:49:30 +0200

    xframe

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
+   after_action :allow_iframe, only: :embed
 
+
+ private
+
+  def allow_iframe
+    response.headers.except! 'X-Frame-Options'
+  end
 end

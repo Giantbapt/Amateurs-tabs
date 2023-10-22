f8a3bba84917730d227071f7cce2c0c26c66a5b6
exec zsh <exec zsh>
2023-10-23 01:02:12 +0200

    remove headers

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,7 @@ module AmateursTabs
     end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.action_dispatch.default_headers.clear
 
     # Configuration for the application, engines, and railties goes here.
     #
